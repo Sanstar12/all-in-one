b684525f5232368bb60e02e907543edacb082e38
Sanstar12 <94879410+Sanstar12@users.noreply.github.com>
2026-05-30 08:45:13 +0530

    Add files via upload

---
+++ devgagan/core/filters.py
@@ -0,0 +1,10 @@
+from pyrogram import filters
+from devgagan.core.mongo.db import get_mode
+
+async def mode_check(f, client, message):
+    user_id = message.from_user.id
+    mode = await get_mode(user_id)
+    return mode == f.mode
+
+archive_filter = filters.create(mode_check, mode="archive")
+restricted_filter = filters.create(mode_check, mode="restricted")
