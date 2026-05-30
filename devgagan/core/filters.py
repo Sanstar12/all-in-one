fead74d931a39e73327ec158ddfdfbe1ba764580
Sanstar12 <94879410+Sanstar12@users.noreply.github.com>
2026-05-30 09:38:18 +0530

    Update filters.py

--- devgagan/core/filters.py
+++ devgagan/core/filters.py
@@ -2,6 +2,8 @@ from pyrogram import filters
 from devgagan.core.mongo.db import get_mode
 
 async def mode_check(f, client, message):
+    if not message.from_user:
+        return False
     user_id = message.from_user.id
     mode = await get_mode(user_id)
     return mode == f.mode
